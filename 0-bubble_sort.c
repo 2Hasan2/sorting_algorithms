34f2d717046448352829bd8b6c7319d6e80f0ba7
2Hasan2 <h01554176846@gmail.com>
2024-02-19 18:00:40 +0200

    2024-02-19 06:00:40 PM

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,21 +10,23 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
-
-	if (array == NULL || size < 2)
-		return;
+	int swapped;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - 1 - i; j++)
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				swapped = 1;
 				print_array(array, size);
 			}
 		}
+		if (swapped == 0)
+			break;
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sorting_algorithms
+
+## compile and run
+```bash
+gcc -Wall -Wextra -Werror -pedantic -std=gnu89 0-bubble_sort.c 0-main.c print_array.c -o 
+```

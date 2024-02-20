4930aa1a97770792cdca43ba41c4e5f0ba552aa1
2Hasan2 <h01554176846@gmail.com>
2024-02-20 22:08:47 +0200

    2024-02-20 10:08:47 PM

--- sort.h
+++ sort.h
@@ -24,7 +24,7 @@ void print_array(const int *array, size_t size);
 
 void bubble_sort(int *array, size_t size);
 
-
+void selection_sort(int *array, size_t size);
 
 void quick_sort(int *array, size_t size);
 void quick_sort_helper(int *array, int low, int high, size_t size);
